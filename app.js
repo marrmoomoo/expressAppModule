710c36abc3eea261b0a1583b5240b588c75a7657
Jen Shannon <jennifer.shannon@galvanize.com>
2017-11-15 11:54:29 -0500

    Add express errors and routes

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+//const path = require('path');
+const bodyParser = require('body-parser');
+const ejs = require('ejs')
+var index = require('./routes/index')
 
-var app = express();
+const app = express();
 
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
+
+app.use('/', index);
+
+app.use((req, res, next)=>{
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(err.status).render('err');
+})
 
 app.listen(8000, function(){
   console.log("You are listening to port 8000")
